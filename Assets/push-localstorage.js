f17ce2ca3fc405f1f30546f67468d58563b65f2e
Hello-AndyJoe <135537100+Hello-AndyJoe@users.noreply.github.com>
2023-07-15 14:15:48 -0700

    Update push-localstorage.js

--- Assets/push-localstorage.js
+++ Assets/push-localstorage.js
@@ -1,8 +1,16 @@
+var allScores = [];
+
 window.onload = function() {
     // Get the value from localStorage
-    var value = localStorage.getItem("playerScoreData");
+    var recoredScore = localStorage.getItem("playerScoreData");
     
+    allScores.push(recoredScore);
+
+    localStorage.setItem("allScores", JSON.stringify(allScores));
+
     // Display the value on the page
-    var recordedHighscore = document.getElementById("highscores-box");
-    recordedHighscore.innerText = value;
+    var recordedHighscores = document.getElementById("highscores-box");
+    recordedHighscores.innerText = allScores;
 };
+
+console.log(allScores);
